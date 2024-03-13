f083fff48c64642fffc67f1b19fad2420424169c
AitanaGS <aitana.graebs@gmail.com>
2024-03-13 16:42:39 +0100

    Clean up string utils

--- src/utils/strings.js
+++ src/utils/strings.js
@@ -1,11 +1,6 @@
 export function splitStringByHalf(inputString) {
-  // Split the input string into an array of words
   const words = inputString.split(' ');
-
-  // Calculate the middle index
   const middleIndex = Math.ceil(words.length / 2);
-
-  // Create two separate arrays
   const firstHalf = words.slice(0, middleIndex);
   const secondHalf = words.slice(middleIndex);
 
@@ -13,22 +8,9 @@ export function splitStringByHalf(inputString) {
 }
 
 export function splitStringOnSlash(inputString) {
-  // return inputString
-  //   .split(/(\s*\/\s*)/)
-  //   .filter((part) => part.trim() !== '')
-  //   .reduce(
-  //     (acc, part, index, array) => (
-  //       index % 2 === 0
-  //         ? acc.push(part + (array[index + 1] || ''))
-  //         : acc,
-  //       acc
-  //     ),
-  //     []
-  //   );
   const parts = inputString.split(/(\s*\/\s*)/);
 
   if (parts.length === 1 || parts.every((part) => part.length <= 6)) {
-    // Return the original string if there is no slash or all parts are <= 6 characters
     return [inputString];
   }
 
@@ -44,34 +26,3 @@ export function splitStringOnSlash(inputString) {
       return acc;
     }, []);
 }
-
-// const containsSlash = /\//.test(inputString);
-
-// const result = containsSlash
-//   ? inputString
-//       .split(/(\s*\/\s*)/)
-//       .filter((part) => part.trim() !== '')
-//       .reduce((acc, part, index, array) => {
-//         if (index % 2 === 0) {
-//           acc.push(part + (array[index + 1] || ''));
-//         }
-//         return acc;
-//       }, [])
-//   : [inputString];
-
-// return result;
-// return /\//.test(inputString)
-//   ? inputString
-//       .split(/\s*\/\s*/)
-//       .filter((part) => part.trim() !== '')
-//   : [inputString];
-// const containsSlash = /\//.test(inputString);
-
-// if (containsSlash) {
-//   const separatedParts = inputString
-//     .split(/\s*\/\s*/)
-//     .filter((part) => part.trim() !== '');
-//   return separatedParts;
-// } else {
-//   return [inputString];
-// }
